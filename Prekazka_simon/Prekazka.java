7731d35e822e7ad9c1b888b1731bd1830bf6d45d
SimonSolik <solik1@stud.uniza.sk>
2025-11-18 08:35:25 +0100

    Zalozenie triedy Prekazka

---
+++ Prekazka_simon/Prekazka.java
@@ -0,0 +1,25 @@
+
+/**
+ * Trieda, ktorá vytvára prekážky do hry
+ * 
+ * @Šimon Solík 
+ * 18.11. 2025
+ */
+public class Prekazka {
+    
+    /**
+     * Constructor for objects of class Prekazka
+     */
+    public Prekazka() {
+        // initialise instance variables
+    }
+    
+    public void urobPZiak(){
+    } 
+    
+    public void urobPAuto(){
+    } 
+    
+    public void urobPZviera(){
+    } 
+}
