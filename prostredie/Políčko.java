fb0276fb84fbec786822cf41fe03ee7a8ccea9bf
TVitko <vitko3@stud.uniza.sk>
2025-11-18 08:34:19 +0100

    zalozenie tried prostredie a policko

---
+++ prostredie/Políčko.java
@@ -0,0 +1,15 @@
+
+/**
+ * Vytvorí 1 políčko z mapy, ktoré sa ďalej používa v Prostredí.
+ * 
+ * @Tomáš Vitko 
+ * @0.1
+ */
+public class Políčko {
+    /**
+     * Constructor for objects of class Políčko
+     */
+    public Políčko() {
+        // initialise instance variables
+    }
+}
