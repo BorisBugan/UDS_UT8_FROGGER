936d46e153ac6ad1cdddbcf468ff99698a3f140d
BorisBugan <bugan20@stud.uniza.sk>
2025-11-18 08:35:50 +0100

    zalozenie triedy Hra Frogg

---
+++ Hra, Frogg/Frogg.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Frogg here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Frogg {
+    /**
+     * Constructor for objects of class Frogg
+     */
+    public Frogg() {
+        // initialise instance variables
+    }
+}
---
+++ Hra, Frogg/Hra.java
@@ -0,0 +1,17 @@
+
+/**
+ * Write a description of class Hra here.
+ * Slúži na spustenie hry, na jej zastavenie a na uloženie skóre.
+ * @author Bugan
+ * @version 1.0 18. 11. 2025
+ */
+public class Hra {
+    /**
+     * Konštruktor pre objekt triedy Hra
+     */
+    public Hra() {
+        // initialise instance variables
+    }
+    
+    public void spustiHru() {}
+}
