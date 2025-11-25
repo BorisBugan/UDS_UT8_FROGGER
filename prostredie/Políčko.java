6e2ed0e09f889c2cf8b554941ba8717183b38455
TVitko <vitko3@stud.uniza.sk>
2025-11-25 08:34:30 +0100

    hotove Policko

--- prostredie/Políčko.java
+++ prostredie/Políčko.java
@@ -1,4 +1,4 @@
-
+import fri.shapesge.Stvorec;
 /**
  * Vytvorí 1 políčko z mapy, ktoré sa ďalej používa v Prostredí.
  * 
@@ -9,7 +9,21 @@ public class Políčko {
     /**
      * Constructor for objects of class Políčko
      */
-    public Políčko() {
-        // initialise instance variables
+    private Stvorec stvorec;
+    //private Frogg frogg;
+    //private Prekazka prekazka;
+    public Políčko(int x, int y, int velkost) {
+        this.stvorec = new Stvorec();
+        this.stvorec.zmenPolohu(x, y);
+        this.stvorec.zmenStranu(velkost);
     }
+    /**
+     
+    public void priradPrekazku(Prekazka prekazka){
+        this.prekazka = prekazka;
+    }
+    public void priradFrogg(Frogg frogg){
+        this.frogg = frogg ;
+    }
+    **/
 }
