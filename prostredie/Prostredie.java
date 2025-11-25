d08346bff12ecce0efb311bdd7ea2fb586e6cf55
TVitko <vitko3@stud.uniza.sk>
2025-11-25 09:12:57 +0100

    hotove Policko a zacate Prostredie

--- prostredie/Prostredie.java
+++ prostredie/Prostredie.java
@@ -9,8 +9,21 @@ public class Prostredie {
     /**
      * Constructor for objects of class Prostredie
      */
-    public Prostredie() {
-        // initialise instance variables
+    private Policko[][] hraciaPlocha;
+    private int sirka;
+    private int vyska;
+    public Prostredie(int vyska, int sirka) {
+        this.vyska = vyska;
+        this.sirka = sirka;
+        
+        hraciaPlocha = new Policko [vyska][sirka];
+        // naplnit default polickami
+        for (int i = 0; i < vyska; i++) {
+            for (int j = 0; j < sirka; j++) {
+                hraciaPlocha[i][j] = new Policko(20,20,20);
+                //DOPLNIT LOGIKU KDE SA TVORIA POLICKA
+            }
+        }
     }
     
     public void urobPStart() {
