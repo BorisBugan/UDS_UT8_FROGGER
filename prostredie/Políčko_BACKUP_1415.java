10f1e0dc69cebb7d6c765cbc47003716ea731ee5
BorisBugan <bugan20@stud.uniza.sk>
2025-12-02 09:23:47 +0100

    policka

---
+++ prostredie/Políčko_BACKUP_1415.java
@@ -0,0 +1,39 @@
+<<<<<<< HEAD
+import fri.shapesge.Stvorec;
+=======
+
+>>>>>>> fb0276f (zalozenie tried prostredie a policko)
+/**
+ * Vytvorí 1 políčko z mapy, ktoré sa ďalej používa v Prostredí.
+ * 
+ * @Tomáš Vitko 
+ * @0.1
+ */
+public class Políčko {
+    /**
+     * Constructor for objects of class Políčko
+     */
+<<<<<<< HEAD
+    private Stvorec stvorec;
+    //private Frogg frogg;
+    //private Prekazka prekazka;
+    public Políčko(int x, int y, int velkost) {
+        this.stvorec = new Stvorec();
+        this.stvorec.zmenPolohu(x, y);
+        this.stvorec.zmenStranu(velkost);
+    }
+    /**
+     
+    public void priradPrekazku(Prekazka prekazka){
+        this.prekazka = prekazka;
+    }
+    public void priradFrogg(Frogg frogg){
+        this.frogg = frogg ;
+    }
+    **/
+=======
+    public Políčko() {
+        // initialise instance variables
+    }
+>>>>>>> fb0276f (zalozenie tried prostredie a policko)
+}
---
+++ prostredie/Políčko_LOCAL_1415.java
@@ -0,0 +1,29 @@
+import fri.shapesge.Stvorec;
+/**
+ * Vytvorí 1 políčko z mapy, ktoré sa ďalej používa v Prostredí.
+ * 
+ * @Tomáš Vitko 
+ * @0.1
+ */
+public class Políčko {
+    /**
+     * Constructor for objects of class Políčko
+     */
+    private Stvorec stvorec;
+    //private Frogg frogg;
+    //private Prekazka prekazka;
+    public Políčko(int x, int y, int velkost) {
+        this.stvorec = new Stvorec();
+        this.stvorec.zmenPolohu(x, y);
+        this.stvorec.zmenStranu(velkost);
+    }
+    /**
+     
+    public void priradPrekazku(Prekazka prekazka){
+        this.prekazka = prekazka;
+    }
+    public void priradFrogg(Frogg frogg){
+        this.frogg = frogg ;
+    }
+    **/
+}
